0b119118e76c20731e05fc49af2a2e4b408cfba9
Danil <bagetov90@mail.ru>
2026-05-20 03:05:52 +0400

    first commit

--- portal_vr/urls.py
+++ portal_vr/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from django.views.static import serve
+from django.urls import re_path
 from django.conf.urls.static import static
 
 urlpatterns = [
@@ -13,4 +15,6 @@ if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 else:
-    pass
+    urlpatterns += [
+        re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
+    ]
